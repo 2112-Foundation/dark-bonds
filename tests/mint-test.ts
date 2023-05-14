639aca3a0619f8425be2896662a4360e595c3661
sausage-dog <wdulkowski@gmail.com>
2023-05-14 16:53:52 +0100

    does something

---
+++ tests/mint-test.ts
@@ -0,0 +1,123 @@
+import { Metaplex, keypairIdentity } from "@metaplex-foundation/js";
+import {
+  Connection,
+  clusterApiUrl,
+  Keypair,
+  LAMPORTS_PER_SOL,
+} from "@solana/web3.js";
+import * as anchor from "@project-serum/anchor";
+import { Program, Wallet } from "@project-serum/anchor";
+// import { MetaplexAnchorNft } from "../target/types/metaplex_anchor_nft";
+import {
+  TOKEN_PROGRAM_ID,
+  createAssociatedTokenAccountInstruction,
+  getAssociatedTokenAddress,
+  createInitializeMintInstruction,
+  MINT_SIZE,
+} from "@solana/spl-token"; // IGNORE THESE ERRORS IF ANY
+const { SystemProgram } = anchor.web3;
+
+describe("mint mate", () => {
+  // Configure the client to use the local cluster.
+  const provider = anchor.AnchorProvider.env();
+  const wallet = provider.wallet as Wallet;
+  anchor.setProvider(provider);
+  //   const program = anchor.workspace
+  // .MetaplexAnchorNft as Program<MetaplexAnchorNft>;
+
+  it("Is initialized!", async () => {
+    // Add your test here.
+
+    const TOKEN_METADATA_PROGRAM_ID = new anchor.web3.PublicKey(
+      "metaqbxxUerdq28cj1RbAWkYQm3ybzjb6a8bt518x1s"
+    );
+
+    const lamports = 10000000000;
+    const getMetadata = async (
+      mint: anchor.web3.PublicKey
+    ): Promise<anchor.web3.PublicKey> => {
+      return (
+        await anchor.web3.PublicKey.findProgramAddress(
+          [
+            Buffer.from("metadata"),
+            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+            mint.toBuffer(),
+          ],
+          TOKEN_METADATA_PROGRAM_ID
+        )
+      )[0];
+    };
+
+    const getMasterEdition = async (
+      mint: anchor.web3.PublicKey
+    ): Promise<anchor.web3.PublicKey> => {
+      return (
+        await anchor.web3.PublicKey.findProgramAddress(
+          [
+            Buffer.from("metadata"),
+            TOKEN_METADATA_PROGRAM_ID.toBuffer(),
+            mint.toBuffer(),
+            Buffer.from("edition"),
+          ],
+          TOKEN_METADATA_PROGRAM_ID
+        )
+      )[0];
+    };
+
+    const mintKey: anchor.web3.Keypair = anchor.web3.Keypair.generate();
+    const NftTokenAccount = await getAssociatedTokenAddress(
+      mintKey.publicKey,
+      wallet.publicKey
+    );
+    console.log("NFT Account: ", NftTokenAccount.toBase58());
+
+    const mint_tx = new anchor.web3.Transaction().add(
+      anchor.web3.SystemProgram.createAccount({
+        fromPubkey: wallet.publicKey,
+        newAccountPubkey: mintKey.publicKey,
+        space: MINT_SIZE,
+        programId: TOKEN_PROGRAM_ID,
+        lamports,
+      }),
+      createInitializeMintInstruction(
+        mintKey.publicKey,
+        0,
+        wallet.publicKey,
+        wallet.publicKey
+      ),
+      createAssociatedTokenAccountInstruction(
+        wallet.publicKey,
+        NftTokenAccount,
+        wallet.publicKey,
+        mintKey.publicKey
+      )
+    );
+  });
+
+  it("Test mint", async () => {
+    const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
+    // const keypair = Keypair.fromSecretKey(
+    //   Buffer.from(JSON.parse(process.env.SOLANA_KEYPAIR!.toString()))
+    // );
+
+    const keypair = anchor.web3.Keypair.generate();
+
+    const metaplex = new Metaplex(connection);
+    metaplex.use(keypairIdentity(keypair));
+
+    const feePayerAirdropSignature = await connection.requestAirdrop(
+      keypair.publicKey,
+      LAMPORTS_PER_SOL
+    );
+    await connection.confirmTransaction(feePayerAirdropSignature);
+
+    //   const mintNFTResponse = await metaplex.nfts().create({
+    //     uri: "https://ffaaqinzhkt4ukhbohixfliubnvpjgyedi3f2iccrq4efh3s.arweave.net/KUAIIbk6p8oo4XHRcq0U__C2r0mwQaNl0gQow4Qp9yk",
+    //     maxSupply: toBigNumber(1),
+    //   });
+
+    //   console.log(mintNFTResponse);
+
+    console.log("THE END");
+  });
+});
